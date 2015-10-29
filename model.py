8c5dd9e8d5348a85a4da32881c23097a0e9b0c97
EnzoGunn <asaeed.email@gmail.com>
2015-10-28 20:22:39 -0700

    add extended event detail model, and unit tests

--- model.py
+++ model.py
@@ -2,12 +2,33 @@
 
 
 class event(object):
-    def __init__(self, alertTime, deviceId, deviceVersion, dstDomain, dstUrl, eventTime, protocolVersion, providerName):
-        self.alertTime = alertTime # e.g. 2013-02-08T11:14:26.0Z
-        self.deviceId = deviceId # e.g.ba6a59f4-e692-4724-ba36-c28132c761de
-        self.deviceVersion = deviceVersion # e.g.13.7a
-        self.dstDomain = dstDomain # e.g.internetbadguys.com
-        self.dstUrl = dstUrl # e.g.http://internetbadguys.com/a-bad-url
-        self.eventTime = eventTime # e.g.2013-02-08T09:30:26.0Z
-        self.protocolVersion = protocolVersion # e.g.1.0a
-        self.providerName = providerName # e.g.Security Platform
+    def __init__(self, alert_time, device_id, device_version, dst_domain, dst_url, event_time, protocol_version, provider_name):
+        self.alertTime = alert_time # e.g. 2013-02-08T11:14:26.0Z
+        self.deviceId = device_id # e.g.ba6a59f4-e692-4724-ba36-c28132c761de
+        self.deviceVersion = device_version # e.g.13.7a
+        self.dstDomain = dst_domain # e.g.internetbadguys.com
+        self.dstUrl = dst_url # e.g.http://internetbadguys.com/a-bad-url
+        self.eventTime = event_time # e.g.2013-02-08T09:30:26.0Z
+        self.protocolVersion = protocol_version # e.g.1.0a
+        self.providerName = provider_name # e.g.Security Platform
+
+class event_detail(event):
+    def __init__(self, alert_time, device_id, device_version, dst_domain, dst_url, event_time, protocol_version, provider_name, disable_dst_safeguards, dst_ip, event_severity, event_type, event_description, event_hash, file_name, file_hash, external_url, src):
+        self.alertTime = alert_time # e.g. 2013-02-08T11:14:26.0Z
+        self.deviceId = device_id # e.g.ba6a59f4-e692-4724-ba36-c28132c761de
+        self.deviceVersion = device_version # e.g.13.7a
+        self.dstDomain = dst_domain # e.g.internetbadguys.com
+        self.dstUrl = dst_url # e.g.http://internetbadguys.com/a-bad-url
+        self.eventTime = event_time # e.g.2013-02-08T09:30:26.0Z
+        self.protocolVersion = protocol_version # e.g.1.0a
+        self.providerName = provider_name # e.g.Security Platform
+        self.disableDstSafeguards = disable_dst_safeguards # e.g. true
+        self.dstIp = dst_ip # e.g. "8.8.8.8"
+        self.eventSeverity = event_severity # e.g. "Severe"
+        self.eventType = event_type # e.g. "Cryptolocker"
+        self.eventDescription = event_description # e.g. "Win32/Crilock.A"
+        self.eventHash = event_hash # e.g. "d41d8cd98f00b204e9800998ecf8427e"
+        self.fileName = file_name # e.g. "%AppData%RoamingMicrosoftWindowsTemplatesrandom.exe"
+        self.fileHash = file_hash # e.g.  "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+        self.externalUrl = external_url # e.g. http://groogle.com/malware.html
+        self.src = src # e.g. 192.168.100.101
--- test/model_test.py
+++ test/model_test.py
@@ -1,30 +1,63 @@
 #! /usr/bin/env python
 
-from model import event
+from model import event, event_detail
 import unittest
+
+alert_time = '2013-02-08T11:14:26.0Z'
+device_id = 'ba6a59f4-e692-4724-ba36-c28132c761de'
+device_version = '13.7a'
+dst_domain = 'internetbadguys.com'
+dst_url = 'http://internetbadguys.com/a-bad-url'
+event_time = '2013-02-08T09:30:26.0Z'
+protocol_version = '1.0a'
+provider_name = 'Security Platform'
+disable_dst_safeguards = True
+dst_ip = '8.8.8.8'
+event_severity = 'Severe'
+event_type = 'Cryptolocker'
+event_description = 'Win32/Crilock.A'
+event_hash = 'd41d8cd98f00b204e9800998ecf8427e'
+file_name = '%AppData%RoamingMicrosoftWindowsTemplatesrandom.exe'
+file_hash = 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+external_url = 'http://groogle.com/malware.html'
+src = '192.168.100.101'
 
 
 class test_event(unittest.TestCase):
+
     def test_event_model(self):
-        alertTime = "2013-02-08T11:14:26.0Z"
-        deviceId = "ba6a59f4-e692-4724-ba36-c28132c761de"
-        deviceVersion = "13.7a"
-        dstDomain = "internetbadguys.com"
-        dstUrl = "http://internetbadguys.com/a-bad-url"
-        eventTime = "2013-02-08T09:30:26.0Z"
-        protocolVersion = "1.0a"
-        providerName = "Security Platform"
+        event_obj = event(alert_time, device_id, device_version, dst_domain, dst_url, event_time, protocol_version, provider_name)
 
-        event_obj = event(alertTime, deviceId, deviceVersion, dstDomain, dstUrl, eventTime, protocolVersion, providerName)
+        self.assertEqual(alert_time, event_obj.alertTime)
+        self.assertEqual(device_id, event_obj.deviceId)
+        self.assertEqual(device_version, event_obj.deviceVersion)
+        self.assertEqual(dst_domain, event_obj.dstDomain)
+        self.assertEqual(dst_url, event_obj.dstUrl)
+        self.assertEqual(event_time, event_obj.eventTime)
+        self.assertEqual(protocol_version, event_obj.protocolVersion)
+        self.assertEqual(provider_name, event_obj.providerName)
 
-        self.assertEqual(alertTime, event_obj.alertTime)
-        self.assertEqual(deviceId, event_obj.deviceId)
-        self.assertEqual(deviceVersion, event_obj.deviceVersion)
-        self.assertEqual(dstDomain, event_obj.dstDomain)
-        self.assertEqual(dstUrl, event_obj.dstUrl)
-        self.assertEqual(eventTime, event_obj.eventTime)
-        self.assertEqual(protocolVersion, event_obj.protocolVersion)
-        self.assertEqual(providerName, event_obj.providerName)
+    def test_event_model(self):
+        event_detail_obj = event_detail(alert_time, device_id, device_version, dst_domain, dst_url, event_time, protocol_version, provider_name)
+
+        self.assertEqual(alert_time, event_detail_obj.alertTime)
+        self.assertEqual(device_id, event_detail_obj.deviceId)
+        self.assertEqual(device_version, event_detail_obj.deviceVersion)
+        self.assertEqual(dst_domain, event_detail_obj.dstDomain)
+        self.assertEqual(dst_url, event_detail_obj.dstUrl)
+        self.assertEqual(event_time, event_detail_obj.eventTime)
+        self.assertEqual(protocol_version, event_detail_obj.protocolVersion)
+        self.assertEqual(provider_name, event_detail_obj.providerName)
+        self.assertEqual(disable_dst_safeguards, event_detail_obj.disableDstSafeguards)
+        self.assertEqual(dst_ip, event_detail_obj.dstIp)
+        self.assertEqual(event_severity, event_detail_obj.eventSeverity)
+        self.assertEqual(event_type, event_detail_obj.eventType)
+        self.assertEqual(event_description, event_detail_obj.eventDescription)
+        self.assertEqual(event_hash, event_detail_obj.eventHash)
+        self.assertEqual(file_name, event_detail_obj.fileName)
+        self.assertEqual(file_hash, event_detail_obj.fileHash)
+        self.assertEqual(external_url, event_detail_obj.externalUrl)
+        self.assertEqual(src, event_detail_obj.src)
 
 
 if __name__ == '__main__':
