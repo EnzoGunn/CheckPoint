61ae6c72f469e820226dc6b1a8b8cd08d4a9ed71
asaeedatsolarcity <asaeed@solarcity.com>
2015-11-02 10:05:24 -0800

    fix version lookup in setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from svc_utils import SvcUtils
 setup(
     name='check_point',
     packages=['check_point'],
-    version=SvcUtils.get_build_version('__init__.py'),
+    version=SvcUtils.get_build_version(),
     description='CheckPoint',
     url='https://github.com/EnzoGunn/CheckPoint',
     author='Ahmad Saeed',
